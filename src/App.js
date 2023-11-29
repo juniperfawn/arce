75a7b5be54db8aa5ce15d2e031c08cda7b1ba6e6
juniperfawn <cerakocher.media@gmail.com>
2023-11-29 19:37:59 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Typewriter, useTypewriter, Cursor } from "react-simple-typewriter";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <div className="content__main">
+          <h1 className="word fancy">
+            <span className="letter">a</span>
+            <span className="letter">r</span>
+            <span className="letter">c</span>
+            <span className="letter">é</span>
+          </h1>
+          <p>NEW YORK</p>
+        </div>
+        <Typewriter
+          words={["coming soon..."]}
+          loop={2}
+          cursor
+          typeSpeed={100}
+          deleteSpeed={80}
+          delaySpeed={1000}
+        />
+      </div>
     </div>
   );
 }
